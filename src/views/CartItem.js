dc2be991440964a124a20e7a18212c83fbfea3bf
naoki-kubota110 <naoki.kubota@rakus-partners.co.jp>
2021-05-28 09:39:05 +0900

    カート一覧に注文入力画面を挿入

--- src/views/CartItem.js
+++ src/views/CartItem.js
@@ -5,6 +5,7 @@ import firebase from 'firebase';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import {Container, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper} from '@material-ui/core';
 import { CART_STATUS_IN } from '../actions/status'
+import Order from '../components/Order'
 
 
 const userSelector = state => state.user.user
@@ -132,6 +133,7 @@ const CartItem = () => {
           </Table>
         </TableContainer>
       </Container>
+      <Order/>
     </>
   )
 }
