bc463e588e338707f62850e0ba19ea5f06952f1d
SonokaOsawa <sooonokaaa@gmail.com>
2021-05-28 16:12:30 +0900

    注文履歴

--- src/views/OrderComplete.js
+++ src/views/OrderComplete.js
@@ -1,5 +1,99 @@
+import React, {useEffect} from "react";
+import {useHistory} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
+import firebase from 'firebase';
+import {CART_STATUS_UNPAID, CART_STATUS_PAID} from '../actions/status';
+import Container from '@material-ui/core/Container';
+import List from "@material-ui/core/List";
+import {makeStyles} from "@material-ui/core/styles";
+import Button from '@material-ui/core/Button';
+import Divider from "@material-ui/core/Divider";
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+
+
+// const userSelector = state => state.user.user
+// const cartSelector = state => state.cart.cart
+
 const OrderComplete = () => {
-  return <div>OrderComplete</div>
+  // const classes = useStyles()
+  const history = useHistory();
+  const handleLink = path => history.push(path);
+  const orders = [
+      {orderdate:'2021-5-28',totalprice:1000,items:[{id:1,buynum:3,size:'M',topping:[{id:1,buynum:3},{id:3,buynum:2}]},
+                                                  {id:3,buynum:2,size:'L',topping:[{id:5,buynum:1}]}]},
+      {orderdate:'2021-5-27',totalprice:2500,items:[{id:7,buynum:3,size:'M',topping:[{id:1,buynum:3},{id:3,buynum:2}]},
+                                                  {id:9,buynum:2,size:'L',topping:[{id:5,buynum:1}]}]}]
+
+  return(
+      <React.Fragment>
+        <Container maxWidth="sm" textAlign="center">
+      <h2>決済が完了しました！</h2>
+      <p>この度はご注文ありがとうございます。</p>
+      <p>お支払い先は、お送りしたメールに記載してありますのでご確認ください。</p>
+      <p>メールが届かない場合は「注文履歴」からご確認ください。</p>
+      <Button variant="outlined" color="secondary" onClick={() => handleLink('/')}>
+      トップ画面に戻る
+      </Button>
+      </Container>
+          {orders.map((order,index) => (
+          <div key={index}>
+            <List>
+              <ListItem>
+          <ListItemText primary={"注文日時：" + order.orderdate}></ListItemText>
+          
+          <ListItemText primary={"お支払い金額：" + order.totalprice}></ListItemText>
+          </ListItem>
+          
+          {order.items.map((item,index) => (
+              <div key={index}>
+                <ListItem>
+              <ListItemText primary={"商品ID：" + item.id}></ListItemText>
+              
+              <ListItemText primary={"数量：" + item.buynum}></ListItemText>
+              
+              {item.topping.map((topping,index) => (
+                <div key={index}>
+                  <ListItemText primary={"トッピングID：" + topping.id}></ListItemText>
+                </div>
+              ))}
+              </ListItem>
+              </div>
+          ))}
+          </List>
+          </div>
+      ))}
+      {/* <section className="c-section-wrapin">
+      <List className={classes.orderList}>
+      {orders.map((order,index) => (
+          <div key={index} className="module-spacer--small">
+      <ListItemText primary={"注文日時：" + order.orderdate}></ListItemText>
+      <p>お支払い金額：{order.totalprice}</p>
+      {order.items.map((item,index) => {
+        
+        <ListItem key={index} className={classes.list}>
+        <ListItemAvatar>
+          <img className={classes.image} src='../pulic/1.jpg'></img>
+        </ListItemAvatar>
+        <div className={classes.text}>
+        <ListItemText
+                primary={"商品名"}
+                secondary={"サイズ：" + item.size}
+              >商品名</ListItemText>
+        <ListItemText
+                primary={"数量：" + item.buynum}
+              />
+        </div>
+        </ListItem>
+        
+      })}
+      
+      </div>))}
+      </List>
+    </section> */}
+      </React.Fragment>
+  )
 }
 
 export default OrderComplete
--- src/views/OrderHistory.js
+++ src/views/OrderHistory.js
@@ -1,5 +1,106 @@
+import React from "react";
+import {CART_STATUS_UNPAID, CART_STATUS_PAID, CART_STATUS_UNDELIVERIED, CART_STATUS_DELIVERIDE, CART_STATUS_CANCEL} from '../actions/status';
+import firebase from 'firebase';
+import {useSelector, useDispatch} from 'react-redux'
+
+const userSelector = state => state.user.user
+const cartSelector = state => state.cart.cart
+
 const OrderHistory = () => {
-  return <div>OrderHistory</div>
+  const user = useSelector(userSelector)
+  const cart = useSelector(cartSelector)
+  const orders = [
+    {status:1,orderdate:'2021-5-28',totalprice:1000,items:[{id:1,buynum:3,size:'M',topping:[{id:1,buynum:3},{id:3,buynum:2}]},
+                                                {id:3,buynum:2,size:'L',topping:[{id:5,buynum:1}]}]},
+    {status:9,orderdate:'2021-5-27',totalprice:2500,items:[{id:7,buynum:3,size:'M',topping:[{id:1,buynum:3},{id:3,buynum:2}]},
+                                                {id:9,buynum:2,size:'L',topping:[{id:5,buynum:1}]}]}]
+     
+    function Orderhistory(){
+      if(orders.length === 0){
+        return <p>注文履歴がありません</p>
+      }else if(orders.length > 0 ){
+          return (
+          // <React.Fragment>
+          //   {orders.filter((order) => {
+          //     return order.status === 1;
+          //   }).map((order,index) => {
+          //     <div key={index}>
+          // <p>注文日時：{order.orderdate}</p>
+          // <p>お支払い金額：{order.totalprice}</p>
+          // {order.items.map((item,index) => (
+          //     <div key={index}>
+          //     <p>商品ID：{item.id}</p>
+          //     <p>数量：{item.buynum}</p>
+          //     {item.topping.map((topping,index) => (
+          //       <div key={index}>
+          //         <p>トッピングID：{topping.id}</p>
+          //       </div>
+          //     ))}
+          //     </div>
+          // ))}
+          // <button>キャンセル</button>
+          // </div>
+          //   })}
+          // </React.Fragment>
+          // )}
+          <React.Fragment>
+          {orders.map((order,index) => (
+          <div key={index}>
+          <p>注文日時：{order.orderdate}</p>
+          <p>お支払い金額：{order.totalprice}</p>
+          <p>商品画像</p>
+          
+          {order.items.map((item,index) => (
+              <div key={index}>
+              <p>商品ID：{item.id}</p>
+              <p>数量：{item.buynum}</p>
+              {item.topping.map((topping,index) => (
+                <div key={index}>
+                  <p>トッピングID：{topping.id}</p>
+
+                </div>
+              ))}
+              </div>
+          ))}
+          
+          <button>キャンセル</button>
+          </div>
+      ))}
+        </React.Fragment>
+      )
+      // else if(orders.length > 0 ){
+        // console.log(orders[count])
+    //     return <React.Fragment>
+    //       {orders.map((order,index) => (
+    //       <div key={index}>
+    //       <p>注文日時：{order.orderdate}</p>
+    //       <p>お支払い金額：{order.totalprice}</p>
+    //       <p>商品画像</p>
+          
+    //       {order.items.map((item,index) => (
+    //           <div key={index}>
+    //           <p>商品ID：{item.id}</p>
+    //           <p>数量：{item.buynum}</p>
+    //           {item.topping.map((topping,index) => (
+    //             <div key={index}>
+    //               <p>トッピングID：{topping.id}</p>
+
+    //             </div>
+    //           ))}
+    //           </div>
+    //       ))}
+    //       </div>
+    //   ))}
+    //     </React.Fragment>
+      }
+    }
+    
+  return (
+    <React.Fragment>
+      <h1>注文履歴</h1>
+      <Orderhistory />
+    </React.Fragment>
+  )
 }
 
 export default OrderHistory
