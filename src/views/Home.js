4874efd6d596449a87344c9eeb3a3dfb21ea6bd7
AtsuyaSakamoto <sakamo.070501@gmail.com>
2021-05-31 11:56:53 +0900

    Home.jsを編集しました。

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,34 +5,46 @@ import { setItem, deleteItem } from "../actions/index";
 import { Button } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import DeleteIcon from "@material-ui/icons/Delete";
-import { withStyles, makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
-import { flexbox } from "@material-ui/system";
-import { red } from "@material-ui/core/colors";
-import { MicNone } from "@material-ui/icons";
+import TextField from "@material-ui/core/TextField";
+// import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
+    // fontSize: 100,
   },
   media: {
-    height: 200,
+    height: 220,
   },
   cardList: {
     display: "flex",
-    // justifyContent: "center",
     flexWrap: "wrap",
     listStyleType: "none",
-    padding: "10px 80px",
+    boxSizing: "borderBox",
+    // justifyContent: "center",
+    // padding: "10px 80px",
+    // margin: "20px",
+    // border: "unset",
+    // textAlign: "center",
   },
   card: {
     width: "25%",
     marginTop: "30px",
+    fontSize: "2px",
+  },
+  input: {
+    margin: "30px 5px 0 0",
+  },
+  buttonSearch: {
+    margin: "45px 5px 0 0",
+  },
+  buttonClear: {
+    margin: "45px 5px 0 0",
   },
 });
 
@@ -57,20 +69,23 @@ const Home = () => {
   const [mozi, setMozi] = useState("");
   const [resultState, setResultState] = useState(false);
   const [karamozi, setKaramozi] = useState(false);
+
   const searchWord = () => {
+    setArray(items);
     let arr = items.filter((item) => {
       return 0 <= item.name.indexOf(mozi);
     });
-    console.log(arr.length);
     const result = arr.length === 0;
     setResultState(result);
     const karaResult = mozi === "";
     setKaramozi(karaResult);
     setArray(arr);
+    if (arr.length === 0) {
+      setArray(items);
+    }
   };
 
   const clearWord = () => {
-    console.log("a");
     setMozi("");
     setResultState(false);
     setArray(items);
@@ -78,39 +93,49 @@ const Home = () => {
   };
   return (
     <div>
-      <div style={{ textAlign: "center", margin: "20px" }}>
-        <input
-          type="text"
+      <div style={{ textAlign: "center" }}>
+        <TextField
+          className={classes.input}
+          id="filled-basic"
+          label="Search Noodle"
+          variant="filled"
+          noValidate
+          autoComplete="off"
           value={mozi}
           onChange={(e) => setMozi(e.target.value)}
-          style={{}}
         />
-
         <Button
+          className={classes.buttonSearch}
           variant="contained"
           color="secondary"
           onClick={searchWord}
-          style={{ margin: "0 3px" }}
         >
           検索
           <SearchIcon />
         </Button>
-        <Button variant="contained" color="secondary" onClick={clearWord}>
+        <Button
+          className={classes.buttonClear}
+          variant="contained"
+          color="secondary"
+          onClick={clearWord}
+        >
           クリア
           <DeleteIcon />
         </Button>
+        {/* </form> */}
       </div>
 
       {resultState && (
         <h2 style={{ textAlign: "center" }}>一致する商品がありません</h2>
       )}
       {karamozi && (
-        <h2 style={{ textAlign: "center" }}>検索キーワード空欄です</h2>
+        <h2 style={{ textAlign: "center" }}>検索キーワードが空欄です</h2>
       )}
 
       <ol className={classes.cardList}>
         {array.map((item) => (
           <li key={item.id} className={classes.card}>
+            {/* <Grid item xs={12}> */}
             <Card className={classes.root}>
               <CardActionArea>
                 <CardMedia className={classes.media}>
@@ -122,19 +147,18 @@ const Home = () => {
                     />
                   </Link>
                 </CardMedia>
-                <br />
                 <CardContent>
                   <Link to={`/item-detail/${item.id}`}>{item.name}</Link>
-                  <p>M {item.priceM}円(税抜き)</p>
-                  <p>L {item.priceL}円(税抜き)</p>
+                  <p
+                  // style={{ fontSize: 20 }}
+                  >
+                    Mサイズ {item.priceM.toLocaleString()}円(税抜き)
+                  </p>
+                  <p>Lサイズ {item.priceL.toLocaleString()}円(税抜き)</p>
                 </CardContent>
               </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  お気に入り
-                </Button>
-              </CardActions>
             </Card>
+            {/* </Grid> */}
           </li>
         ))}
       </ol>
